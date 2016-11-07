d381de45c26a1d4384fcb17d31d91c49f0b55a30
RLopez <romariolopezc@gmail.com>
2016-11-07 11:30:54 -0600

    Pre, In, and Post order traversals added (iterative and recursive) BFS added

--- hackerrank/trees.rb
+++ hackerrank/trees.rb
@@ -1,35 +1,86 @@
 class Trees
-  def bst?(node)
 
+  def preorder_recursive(node)
+    if node != nil
+      puts node.data # Visit node
+      preorder_recursive(node.left)
+      preorder_recursive(node.right)
+    end
   end
 
-  def inorder_traversal(root)
+  def preorder_iterative(node)
     s = [] # Stack
-    current = root
+    s.push(node)
+    while !s.empty?
+      current = s.pop
+      puts current.value # Visit node
+      s.push(current.right) if current.right != nil
+      s.push(current.left) if current.left != nil
+    end
+  end
 
-    while !s.empty? || current != nil
-      if current
-        s.push(current)
-        current = current.left
+  def inorder_recursive(node)
+    if node != nil
+      inorder_recursive(node.left)
+      puts node.data # Visit node
+      inorder_recursive(node.right)
+    end
+  end
+
+  def inorder_iterative(node)
+    s = [] # Stack
+    while !s.empty? || node != nil
+      if node != nil
+        s.push(node)
+        node = node.left
       else
-        current = s.pop
-        puts current.value
-        current = current.right
+        node = s.pop
+        puts node.value
+        node = node.right
       end
     end
   end
 
-  def preorder_traversal(root)
-    s = []
-    current = root
-    while !s.empty? || current != nil
-      current = s.pop
-      puts current.value
-      if current.right != nil
-        s.push(current.right)
+  def postorder_recursive(node)
+    if node != nil
+      postorder_recursive(node.left)
+      postorder_recursive(node.right)
+      puts node.data #Visit node
+    end
+  end
+
+  def postorder_iterative(node)
+    s = [] # Stack
+    last_node_visited = nil
+    while !s.empty || node != nil
+      if node != nil
+        s.push(node)
+        node = node.left
+      else
+        peek = s.last
+        if peek.right != nil && last_node_visited != peek.right
+          node = peek.right
+        else
+          puts peek.data # Visit node
+          last_node_visited = s.pop
+        end
       end
-      if current.left != nil
-        s.push(current.left)
+    end
+  end
+
+
+  def BFS(node)
+    queue = []
+    current = node
+    queue.push(current)
+    while !queue.empty?
+      node = queue.shift
+      puts node.data
+      if node.left
+        queue.push(node.left)
+      end
+      if node.right
+        queue.push(node.right)
       end
     end
   end
