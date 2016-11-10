912e9d31c921f9a780a2df7a0a33021d5a6186cf
RLopez <romariolopezc@gmail.com>
2016-11-10 16:26:51 -0600

    Actualización de los ejercicios de HackerRank

--- hackerrank/arrays.rb
+++ hackerrank/arrays.rb
@@ -1,5 +1,44 @@
 class Arrays
 
+  def reverse(array)
+    array.reverse_each do |i|
+      print "#{i} "
+    end
+  end
+
+  def 2d_array_hourglass(array)
+    max = nil
+    4.times do |i|
+      4.times do|j|
+        sum = array[i][j] + array[i][j+1] + array[i][j+2] + array[i+1][j+1] + array[i+2][j] + array[i+2][j+1] + array[i+2][j+2]
+        if max == nil
+          max = sum
+        else
+          max = [max, sum].max
+        end
+      end
+    end
+    puts max
+  end
+
+  def left_rotation(array, steps)
+    steps.times do
+      array.push(array.shift)
+    end
+    puts array.join(' ')
+  end
+
+  def sparse_arrays(array)
+    strings = Hash.new(0)
+
+    gets.chomp.to_i.times do
+      strings[gets.chomp] += 1
+    end
+
+    gets.chomp.to_i.times do
+      puts strings[gets.chomp]
+    end
+  end
   # Time  O(n)
   # Space O(n)
   def self.rotate_1(array, steps)
